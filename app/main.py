077aacc890cf021a1c3781ec41711a7d0c7f773d
DHomiller <71155815+DHomiller@users.noreply.github.com>
2025-03-08 23:53:49 -0600

    dash UI

--- app/main.py
+++ app/main.py
@@ -10,142 +10,29 @@ from opencage.geocoder import OpenCageGeocode
 
 app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/Figure-Friday/main/2024/week-32/irish-pay-gap.csv')
-df['Report Link'] = df['Report Link'].apply(lambda x: f'[Report]({x})')
-df['Company'] = df.apply(lambda row: f'[{row["Company Name"]}]({row["Company Site"]})', axis=1)
-df.rename(columns={'Q1 Men': 'Q1 Male'}, inplace=True)
+df = pd.read_csv('data/crisis_counts.csv')
+df = df[df['severity'] > 0.0]
 
-numeric_columns = [
-   'Mean Hourly Gap', 'Median Hourly Gap', 'Mean Bonus Gap', 'Median Bonus Gap', 'Mean Hourly Gap Part Time',
-   'Median Hourly Gap Part Time', 'Mean Hourly Gap Part Temp', 'Median Hourly Gap Part Temp', 'Percentage Bonus Paid Female',
-   'Percentage Bonus Paid Male', 'Percentage BIK Paid Female', 'Percentage BIK Paid Male', 'Q1 Female', 'Q1 Male', 'Q2 Female',
-   'Q2 Male', 'Q3 Female', 'Q3 Male', 'Q4 Female', 'Q4 Male', 'Percentage Employees Female', 'Percentage Employees Male'
-]
-
-key = '72aeb073b8634354b6820656ddd13018'
+key = ''
 geocoder = OpenCageGeocode(key)
 
-query = u'North Carolina'
-results = geocoder.geocode(query)
+mapchildren = [dl.TileLayer()]
 
-print(u'%f;%f;%s;%s' % (results[0]['geometry']['lat'],
-                        results[0]['geometry']['lng'],
-                        results[0]['components']['country_code'],
-                        results[0]['annotations']['timezone']['name']))
+for i, row in df.iterrows():
+    loc = row['locations']
+    result = geocoder.geocode(loc)
+    coords = result[0]['geometry']
+    severity = row['severity']
+    mapchildren.append(dl.CircleMarker(center=coords, radius=severity*3+8, children=[dl.Tooltip(row['disasters'])]))
 
-initial_location = results[0]['geometry']
+map = dl.Map(center=[39.01, -98.48], zoom=4, style={'width': '50%', 'height': '500px'}, children=mapchildren, id="map")
 
-tweet_text_title = html.H3("Tweet Text",className="bg-secondary text-white p-2 mb-4")
-
-tweet_text = html.P("PHOTOS: Deadly wildfires rage in California")
-
-tweet_info_title = html.H3("Tweet Info",className="bg-secondary text-white p-2 mb-4")
-
-tweet_info = html.P("IS A DISASTER")
-
-tweet_info2 = html.P("IS NOT HUMANITARIAN")
-
-tweet_info3 = html.P("SEVERITY: Severe")
-
-title_string = "Extracted Tweet Location"
-
-map_title = html.H3(title_string,className="bg-secondary text-white p-2 mb-4")
-
-map = dl.Map(center=initial_location, zoom=4, style={'width': '50%', 'height': '500px'}, children=[
-        dl.TileLayer(),  # Adds the map tiles
-        dl.Marker(position=initial_location)  # Initial marker (Dublin)
-    ], id="map")
-
-
-company_dropdown = html.Div(
-    [
-        dbc.Label("Select a Company", html_for="company_dropdown"),
-        dcc.Dropdown(
-            id="company-dropdown",
-            options=sorted(df["Company Name"].unique()),
-            value='Ryanair',
-            clearable=False,
-            maxHeight=600,
-            optionHeight=50
-        ),
-    ],  className="mb-4",
-)
-
-year_radio = html.Div(
-    [
-        dbc.Label("Select Year", html_for="date-checklist"),
-        dbc.RadioItems(
-            options=[2023, 2022],
-            value=2023,
-            id="year-radio",
-        ),
-    ],
-    className="mb-4",
-)
-
-control_panel = dbc.Card(
-    dbc.CardBody(
-        [year_radio, company_dropdown ],
-        className="bg-light",
-    ),
-    className="mb-4"
-)
-
-heading = html.H1("Crisis Analysis Single Tweet View",className="bg-secondary text-white p-2 mb-4")
-
-about_card = dcc.Markdown(
-    """
-    This is a demo of our crisis analysis model. The UI and model are in progress.
-    """)
-
-data_card = dcc.Markdown(
-    """
-    Starting from 2022, Gender Pay Gap Reporting is a regulatory requirement that mandates employers in Ireland with
-     more than 250 employees to publish information on their gender pay gap.
-     
-     [Data source](https://paygap.ie/)
-     
-     [Data source GitHub](https://github.com/zenbuffy/irishGenderPayGap/tree/main)
-     
-     This site was created for Plotly's Figure Friday challenge. For additional data visualizations of this dataset and
-      to join the conversation, visit the [Plotly Community Forum](https://community.plotly.com/t/figure-friday-2024-week-32/86401)
-    """
-)
-
-info = dbc.Accordion([
-    dbc.AccordionItem(about_card, title="About Crisis Analysis", ),
-    dbc.AccordionItem(data_card, title="Data Source")
-],  start_collapsed=True)
-
-def make_grid():
-    grid = dag.AgGrid(
-        id="grid",
-        rowData=df.to_dict("records"),
-        columnDefs=[
-          {"field": "Company", "cellRenderer": "markdown", "linkTarget": "_blank",  "initialWidth":250, "pinned": "left" },
-          {"field": "Report Link", "cellRenderer": "markdown", "linkTarget": "_blank", "floatingFilter": False},
-          {"field": "Report Year" }] +
-        [{"field": c} for c in numeric_columns],
-        defaultColDef={"filter": True, "floatingFilter": True,  "wrapHeaderText": True, "autoHeaderHeight": True, "initialWidth": 125 },
-        dashGridOptions={},
-        filterModel={'Report Year': {'filterType': 'number', 'type': 'equals', 'filter': 2023}},
-        rowClassRules = {"bg-secondary text-dark bg-opacity-25": "params.node.rowPinned === 'top' | params.node.rowPinned === 'bottom'"},
-        style={"height": 600, "width": "100%"}
-    )
-    return grid
-
+heading = html.H1("Crisis Analysis Map View",className="bg-secondary text-white p-2 mb-4")
 
 app.layout = dbc.Container(
     [
         dcc.Store(id="store-selected", data={}),
         heading,
-        tweet_text_title,
-        tweet_text,
-        tweet_info_title,
-        tweet_info,
-        tweet_info2,
-        tweet_info3,
-        map_title,
         map
     ],
     fluid=True,
